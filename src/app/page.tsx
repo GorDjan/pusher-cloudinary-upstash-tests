a015e84689a9885e45cb7b2135eac4199e6dbc41
GorDjan <gorduryan7@gmail.com>
2024-09-12 14:51:45 +0400

    ui done, pj ready for auth & db connections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ChatLayout from "@/components/chat/ChatLayout";
 import PreferencesTab from "@/components/PreferencesTab";
-
+import { cookies } from "next/headers";
 
 export default function Home() {
+
+	const layout = cookies().get("react-resizable-panels:layout");
+	const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   return (
 	<main className='flex h-screen flex-col items-center justify-center p-4 md:px-24 py-32 gap-4'>
 			
@@ -11,8 +15,11 @@ export default function Home() {
 				className='absolute top-0 z-[-2] h-screen w-screen dark:bg-[#000000] dark:bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] 
 				dark:bg-[size:20px_20px] bg-[#ffffff] bg-[radial-gradient(#00000033_1px,#ffffff_1px)] bg-[size:20px_20px]'
 				aria-hidden='true'
-			/>
-
+			/>	
+			
+			<div className='z-10 border rounded-lg max-w-5xl w-full min-h-[85vh] text-sm lg:flex'>
+			<ChatLayout defaultLayout={defaultLayout} users={[]} />
+			</div>
 		</main>
   );
 }
