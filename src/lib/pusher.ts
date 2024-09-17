8c0985b64d5c49f369c9663b7728a3e1ecfffb5d
GorDjan <gorduryan7@gmail.com>
2024-09-17 18:52:41 +0400

    puhser connected,project done

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,8 +1,8 @@
-//import PusherServer from "pusher";
-//import PusherClient from "pusher-js";
+import PusherServer from "pusher";
+import PusherClient from "pusher-js";
 
-//// in development this will create multiple instances of pusher,
-//// which might cause you to hit the connection limit in free tier
+// in development this will create multiple instances of pusher,
+// which might cause you to hit the connection limit in free tier
 // export const pusherServer = new PusherServer({
 //   appId: process.env.PUSHER_APP_ID!,
 //   key: process.env.PUSHER_APP_KEY!,
@@ -15,20 +15,20 @@
 //   cluster:"eu",
 // })
 
-//declare global {
-//	var pusherServer: PusherServer | undefined;
-//	var pusherClient: PusherClient | undefined;
-//}
+declare global {
+	var pusherServer: PusherServer | undefined;
+	var pusherClient: PusherClient | undefined;
+}
 
-//export const pusherServer =
-//	global.pusherServer ||
-//	new PusherServer({
-//		appId: process.env.PUSHER_APP_ID!,
-//		key: process.env.PUSHER_APP_KEY!,
-//		secret: process.env.PUSHER_APP_SECRET!,
-//		cluster: "eu",
-//		useTLS: true,
-//	});
+export const pusherServer =
+	global.pusherServer ||
+	new PusherServer({
+		appId: process.env.PUSHER_APP_ID!,
+		key: process.env.PUSHER_APP_KEY!,
+		secret: process.env.PUSHER_APP_SECRET!,
+		cluster: "eu",
+		useTLS: true,
+	});
 
-//export const pusherClient =
-//	global.pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, { cluster: "eu" });
+export const pusherClient =
+	global.pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, { cluster: "eu" });
